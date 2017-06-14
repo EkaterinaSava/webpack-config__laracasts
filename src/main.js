4faeb129e42b43fa8bff5dc44676b75b7b034391
Kate K <ek@weare.lt>
2017-06-14 17:36:51 +0300

    ADD some loaders - "babel-loader", "babel-core", "babel-preset-es2015" && ADD "babel-config" (.babelrc file), UPD "webpack.config" for this loaders

--- src/main.js
+++ src/main.js
@@ -1,22 +1,12 @@
 'use strict';
 
-//__способы импортировать, исходя из саособа экспорта (см.файл notify.js)
-//__1.
-//import notify from './Notify';
+//__например, добавим класс Form и посмотрим, что с ним сделает babel при сборке
+class Form {
 
-//__2.
-//import {notify, log} from './Notify';
+  constructor() {
+    alert('Yay!');
+  }
 
-//__3.
-import notify from './Notify';
+}
 
-// notify('Hello, I\'m alert');
-// log('Hello, I\'m console log')
-
-//__для третьего случая мы получим объект, поэтому обращаться к функциям внутри будем как к методам объекта
-//console.log(notify);
-notify.announce('I\'m alert');
-notify.log('I\'m console log');
-
-//__подключим css:
-require('./main.css');
+new Form();
--- webpack.config.js
+++ webpack.config.js
@@ -14,10 +14,18 @@ module.exports = {
   module: {
 
     rules: [
+
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader']
+      },
+
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
       }
+
     ]
 
   }
