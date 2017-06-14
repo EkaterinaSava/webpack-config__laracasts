e45075c40d871a68fbe62690cddb89de4836d985
Kate K <ek@weare.lt>
2017-06-14 18:14:45 +0300

    ADD "sass-loader" & "node-sass" && ADD "main.scss" file && UPD "webpack.config" for parse our scss-stuff

--- src/main.js
+++ src/main.js
@@ -1,12 +1,3 @@
 'use strict';
 
-//__например, добавим класс Form и посмотрим, что с ним сделает babel при сборке
-class Form {
-
-  constructor() {
-    alert('Yay!');
-  }
-
-}
-
-new Form();
+require('./main.scss');
--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,11 @@ module.exports = {
     rules: [
 
       {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+
+      {
         test: /\.css$/,
         use: ['style-loader', 'css-loader']
       },
